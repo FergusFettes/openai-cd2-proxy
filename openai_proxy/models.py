12cd0b2f5994393fcb753fa4dbecac689132ae16
fergus <fergusfettes@gmail.com>
2023-11-25 11:52:23 +0000

    added some usage functions

--- openai_proxy/models.py
+++ openai_proxy/models.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta
 import uuid
 from tortoise import Tortoise, fields
 from tortoise.models import Model
+from tortoise import functions
 
 
 class APIKey(Model):
@@ -46,6 +48,34 @@ async def db_task(func, *args, **kwargs):
         await func(*args, **kwargs)
     finally:
         await Tortoise.close_connections()
+
+
+# Function to calculate total token usage for a given name
+async def get_total_usage(name: str):
+    total = await Usage.filter(name=name).annotate(total=functions.Sum('tokens')).values('total')
+    return total[0].get('total')
+
+
+# Function to calculate usage per given timeframe for a given name
+async def get_usage_per_timeframe(name: str, timeframe: str):
+    now = datetime.utcnow()
+    if timeframe == 'day':
+        start_time = now - timedelta(days=1)
+    elif timeframe == 'week':
+        start_time = now - timedelta(weeks=1)
+    elif timeframe == 'hour':
+        start_time = now - timedelta(hours=1)
+    elif timeframe == 'minute':
+        start_time = now - timedelta(minutes=1)
+    else:
+        raise ValueError('Invalid timeframe provided.')
+
+    total = await (
+        Usage
+        .filter(name=name, time__gte=start_time.timestamp())
+        .annotate(total=functions.Sum('tokens')).values('total')
+    )
+    return total[0].get('total')
 
 
 @cli.command()
@@ -97,6 +127,27 @@ async def usage():
     usages = await Usage.all()
     for usage in usages:
         typer.echo(f"{usage.name}: {usage.time}")
+
+
+# CLI command to get total usage
+@cli.command()
+@with_db
+async def total_usage(name: str):
+    total = await get_total_usage(name)
+    typer.echo(f"Total tokens used by {name}: {total}")
+
+
+# CLI command to get usage per timeframe
+@cli.command()
+@with_db
+async def usage_per_timeframe(name: str, timeframe: str = "day"):
+    timeframe = timeframe.lower()
+    valid_timeframes = ["day", "week", "hour", "minute"]
+    if timeframe not in valid_timeframes:
+        typer.echo(f"Invalid timeframe. Choose from {valid_timeframes}.")
+        raise typer.Abort()
+    total = await get_usage_per_timeframe(name, timeframe)
+    typer.echo(f"Tokens used by {name} per {timeframe}: {total}")
 
 
 if __name__ == "__main__":
